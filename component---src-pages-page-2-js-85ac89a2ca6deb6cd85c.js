9ed272886cffc4a89e3f321abfb8fc13500116b2
LuisArmando-TestCoder <luisarmando.murillobaltodano@gmail.com>
2019-10-05 01:42:46 -0600

    Updates

--- component---src-pages-page-2-js-85ac89a2ca6deb6cd85c.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{210:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),l=a(56),c=a(207),r=a(206);t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
-//# sourceMappingURL=component---src-pages-page-2-js-85ac89a2ca6deb6cd85c.js.map
---
+++ styles-8b0a72535f5ff4965c3a.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{179:function(n,o,w){},213:function(n,o,w){},216:function(n,o,w){}}]);
+//# sourceMappingURL=styles-8b0a72535f5ff4965c3a.js.map
